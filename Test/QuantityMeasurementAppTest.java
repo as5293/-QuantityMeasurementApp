93b1758f51144a73663e036cfd375f19eee09739
https://github.com/as5293 <as5293@srmist.edu.in>
2026-04-15 10:10:20 +0530

    UC1: Feet measurement equality

---
+++ Test/QuantityMeasurementAppTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuantityMeasurementAppTest {
+
+
+    @Test
+    public void testEquality_SameValue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(f1.equals(f2));
+    }
+
+    @Test
+    public void testEquality_DifferentValue() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(2.0);
+
+        assertFalse(f1.equals(f2));
+    }
+
+    @Test
+    public void testEquality_NullComparison() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertFalse(f1.equals(null));
+    }
+
+    @Test
+    public void testEquality_DifferentClass() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+        String s = "1.0";
+
+        assertFalse(f1.equals(s));
+    }
+
+    @Test
+    public void testEquality_SameReference() {
+        QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(f1.equals(f1));
+    }
+}
